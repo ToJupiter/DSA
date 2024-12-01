811c2f44dc3d961b785f3c8bc064b936c47583d7
root <root@DESKTOP-QCLP3Q3>
2024-12-01 14:30:48 +0700

    More Backtracking

---
+++ BasicCProgramming/Backtracking/BinarySequence.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int n;
+
+void print_binary(int n, char binary[]){
+    for (int i = 0; i < n; i++){
+        printf("%c", binary[i]);
+    }
+    printf("\n");
+}
+
+void generator(int step, int n, char binary[]){
+    if (step==n){
+        print_binary(n, binary);
+        return;
+    }
+
+    // Theo cach lam backtracking binh thuong, chung ta SE UNDO CHANGES DONE.
+    // tuy nhien voi bai binary seq, viec undo 0 dong nghia voi replace '0' bang '1'.
+    // do vay, binary[step] = '1'.
+    binary[step] = '0';
+    generator(step + 1, n, binary);
+
+    binary[step] = '1';
+    generator(step + 1, n, binary);
+    
+}
+
+int main(){
+    scanf("%d", &n); char binary[100];
+    generator(0, n,binary);
+    
+
+}
